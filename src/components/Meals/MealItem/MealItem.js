3a9004f7eeb1821e34823a205ee3e3a89baf4b90
seyitahmetinci <seyitahmetinci@gmail.com>
2023-08-14 12:26:26 +0300

    Refs & Forward Refs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,18 +1,30 @@
 import classes from "./MealItem.module.css";
 import MealItemForm from "../MealForm/MealItemForm"
+import {useContext} from "react";
+import CardContext from "../../../store/card-context";
 
 const MealItem = (props) => {
-    const price = `$${props.meal.price.toFixed(2)}`;
+    const cartCtx = useContext(CardContext);
+    const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
 
   return(
       <li className={classes.meal}>
           <div>
               <h3>{props.name}</h3>
-              <div className={classes.description}>{props.meal.description}</div>
+              <div className={classes.description}>{props.description}</div>
               <div className={classes.price}>{price}</div>
           </div>
           <div>
-              <MealItemForm id={props.id}/>
+              <MealItemForm id={props.id} onAddToCart={addToCartHandler}/>
           </div>
       </li>
   );
--- src/store/CardProvider.js
+++ src/store/CardProvider.js
@@ -1,16 +1,43 @@
 import CartContext from "./card-context";
+import {useReducer} from "react";
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state,action) => {
+    if (action.type === 'ADD'){
+        //concat works like map but creates new array instead of pushing
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+    else  if (action.type === 'REMOVE'){
+
+    }
+  return defaultCartState;
+};
 
 const CardProvider = (props) => {
-    const addItemToCartHandler = item => {};
+    const [carState,dispatchCartAction ] = useReducer(cartReducer, defaultCartState);
+    const addItemToCartHandler = item => {
+        dispatchCartAction({type: 'ADD', item: item});
+    };
 
-    const removeItemFromCartHandler = id => {};
+    const removeItemFromCartHandler = id => {
+        dispatchCartAction({type: 'REMOVE', id:id});
+    };
 
 
     const cartContext = {
-        items:[],
-        totalAmount:0,
+        items:carState.items,
+        totalAmount:carState.totalAmount,
         addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
+        removeItem: removeItemFromCartHandler,
     };
 
     return(<CartContext.Provider value={cartContext}>
